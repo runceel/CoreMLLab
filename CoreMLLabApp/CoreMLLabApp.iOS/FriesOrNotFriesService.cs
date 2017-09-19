1d7db8d54fd272a8f66a7e330110ab3bfec6a54b
Kazuki Ota <kaota@microsoft.com>
2017-09-19 22:46:31 +0900

    プロジェクト ファイルを追加します。

---
+++ CoreMLLabApp/CoreMLLabApp.iOS/FriesOrNotFriesService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Foundation;
+using Vision;
+using CoreML;
+using CoreImage;
+using CoreFoundation;
+
+[assembly: Xamarin.Forms.Dependency(typeof(CoreMLLabApp.iOS.FriesOrNotFriesService))]
+namespace CoreMLLabApp.iOS
+{
+    public class FriesOrNotFriesService : IFriesOrNotFriesService
+    {
+        private static VNCoreMLModel VModel { get; }
+
+        static FriesOrNotFriesService()
+        {
+            // Load the ML model
+            var assetPath = NSBundle.MainBundle.GetUrlForResource("e3e4e645c0944c6ca84f9a000e501b22", "mlmodelc");
+            var friedOrNotFriedModel = MLModel.Create(assetPath, out _);
+            VModel = VNCoreMLModel.FromMLModel(friedOrNotFriedModel, out _);
+        }
+
+        public Task<string> DetectAsync(byte[] image)
+        {
+            var taskSource = new TaskCompletionSource<string>();
+            void handleClassification(VNRequest request, NSError error)
+            {
+                var observations = request.GetResults<VNClassificationObservation>();
+                if (observations == null)
+                {
+                    taskSource.SetException(new Exception("Unexpected result type from VNCoreMLRequest"));
+                    return;
+                }
+
+                if (observations.Length == 0)
+                {
+                    taskSource.SetResult(null);
+                    return;
+                }
+
+                var best = observations.First();
+                taskSource.SetResult(best.Identifier);
+            }
+
+            using (var data = NSData.FromArray(image))
+            {
+                var ciImage = new CIImage(data);
+                var handler = new VNImageRequestHandler(ciImage, new VNImageOptions());
+                DispatchQueue.DefaultGlobalQueue.DispatchAsync(() =>
+                {
+                    handler.Perform(new VNRequest[] { new VNCoreMLRequest(VModel, handleClassification) }, out _);
+                });
+            }
+
+            return taskSource.Task;
+        }
+
+    }
+}
---
+++ CoreMLLabApp/CoreMLLabApp/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.IO;
+using Xamarin.Forms;
+
+namespace CoreMLLabApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions());
+            if (file == null) { return; }
+
+            this.image.Source = ImageSource.FromStream(() => file.GetStream());
+
+            using (var fs = file.GetStream())
+            using (var ms = new MemoryStream())
+            {
+                await fs.CopyToAsync(ms);
+                var d = DependencyService.Get<IFriesOrNotFriesService>();
+                var result = await d.DetectAsync(ms.ToArray());
+                await this.DisplayAlert("Result", result, "OK");
+            }
+        }
+    }
+}
